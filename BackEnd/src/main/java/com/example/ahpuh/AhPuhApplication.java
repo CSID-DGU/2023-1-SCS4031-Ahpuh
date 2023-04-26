2ec8588fa44d1ae4673034740030a2961a586658
unknown <codusl100@naver.com>
2023-04-26 15:30:09 +0900

    chore: 백엔드 환경 구축

---
+++ BackEnd/src/main/java/com/example/ahpuh/AhPuhApplication.java
@@ -0,0 +1,13 @@
+package com.example.ahpuh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhPuhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhPuhApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/example/ahpuh/AhPuhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ahpuh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhPuhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

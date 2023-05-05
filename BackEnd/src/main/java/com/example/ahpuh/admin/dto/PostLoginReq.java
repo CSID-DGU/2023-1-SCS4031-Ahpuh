c27990e0a5674fc493c48fcbe0fa315d7034c112
백채연 <codusl100@naver.com>
2023-05-05 16:41:29 +0900

    docs : 회원가입, 로그인 API Swagger 추가

--- BackEnd/src/main/java/com/example/ahpuh/admin/dto/PostLoginReq.java
+++ BackEnd/src/main/java/com/example/ahpuh/admin/dto/PostLoginReq.java
@@ -1,4 +1,5 @@
 package com.example.ahpuh.admin.dto;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Getter
@@ -6,6 +7,8 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostLoginReq {
+    @ApiModelProperty(example = "이메일")
     private String email;
+    @ApiModelProperty(example = "비밀번호")
     private String pwd;
 }

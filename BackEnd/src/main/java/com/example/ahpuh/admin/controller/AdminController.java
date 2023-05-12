90c3d68df4e63f43b4ead6fec3584bef3c73fb10
백채연 <codusl100@naver.com>
2023-05-12 10:31:55 +0900

    feat : 관리자 초기 이미지 업로드 & 엔티티 수정

--- BackEnd/src/main/java/com/example/ahpuh/admin/controller/AdminController.java
+++ BackEnd/src/main/java/com/example/ahpuh/admin/controller/AdminController.java
@@ -1,18 +1,25 @@
 package com.example.ahpuh.admin.controller;
 
 import com.example.ahpuh.admin.dto.PostAdminReq;
+import com.example.ahpuh.admin.dto.PostAdminRes;
+import com.example.ahpuh.admin.dto.PostCctvImgRes;
 import com.example.ahpuh.admin.dto.PostLoginReq;
 import com.example.ahpuh.admin.service.AdminService;
+import com.example.ahpuh.admin.service.S3Service;
 import com.example.ahpuh.jwt.dto.TokenDto;
+import com.example.ahpuh.jwt.service.JwtService;
 import com.example.ahpuh.util.BaseException;
 import com.example.ahpuh.util.BaseResponse;
 import com.example.ahpuh.util.BaseResponseStatus;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,14 +30,18 @@ public class AdminController {
 
     @Autowired
     private AdminService adminService;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private S3Service s3Service;
 
     @ResponseBody
     @ApiOperation(value = "회원가입", notes ="비밀번호 validation 규칙은 8글자 이상 16글자 이하/문자 + 숫자 섞어서")
     @PostMapping("/sign-up")
-    public BaseResponse<TokenDto> signUp(@RequestBody PostAdminReq admin) {
+    public BaseResponse<String> signUp(@RequestBody PostAdminReq admin) {
         try {
-            TokenDto token = adminService.signUp(admin);
-            return new BaseResponse<>(token);
+            adminService.signUp(admin);
+            return new BaseResponse<>("회원가입에 성공하였습니다.");
 
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
@@ -40,16 +51,27 @@ public class AdminController {
     @ResponseBody
     @ApiOperation(value = "로그인", notes ="이메일, 비밀번호 입력")
     @PostMapping("/sign-in")
-    public BaseResponse<TokenDto> signIn(@RequestBody PostLoginReq admin) {
+    public BaseResponse<PostAdminRes> signIn(@RequestBody PostLoginReq admin) {
         if (admin.getEmail().length() == 0 || admin.getEmail() == null) {
             return new BaseResponse<>(BaseResponseStatus.POST_ADMIN_EMPTY_EMAIL);
         }
         try {
-            TokenDto token = adminService.signIn(admin);
+            PostAdminRes token = adminService.signIn(admin);
             return new BaseResponse<>(token);
-
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
     }
+
+    @Operation(summary = "post cctv Img", description = "관리자 초기 설정 - cctv 이미지 업로드")
+    @Parameter(name = "cctvImg", description = "업로드할 cctv 이미지", example = "cctv 이미지")
+    @ResponseBody
+    @PostMapping("/cctv/img")
+    public BaseResponse<PostCctvImgRes> uploadImg(@RequestPart(value = "cctvImg") MultipartFile multipartFile) throws BaseException {
+        String cctvImg = s3Service.uploadImg(multipartFile);
+        PostCctvImgRes postCctvImgRes = adminService.uploadCctvImg(cctvImg);
+        return new BaseResponse<>(postCctvImgRes);
+    }
+
+
 }
---
+++ BackEnd/src/main/java/com/example/ahpuh/admin/dto/PostAdminRes.java
@@ -0,0 +1,15 @@
+package com.example.ahpuh.admin.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostAdminRes {
+    private Long id;
+    private String jwt;
+}

a3f6257569192a59c3ad47f115588c6fc72d82bc
백채연 <codusl100@naver.com>
2023-06-02 20:40:03 +0900

    refactor : uri 수정

--- BackEnd/src/main/java/com/example/ahpuh/cctv/controller/CctvController.java
+++ BackEnd/src/main/java/com/example/ahpuh/cctv/controller/CctvController.java
@@ -29,7 +29,7 @@ public class CctvController {
     @Operation(summary = "post cctv Img & video", description = "관리자 초기 설정 - cctv 이미지, 비디오 업로드")
     @Parameter(name = "cctvFile", description = "업로드할 cctv 이미지와 동영상", example = "cctv 이미지, 동영상")
     @ResponseBody
-    @PostMapping("/cctv/file")
+    @PostMapping("/file")
     public BaseResponse<PostCctvFileRes> uploadFile(@RequestPart(value = "cctvFile") MultipartFile multipartFile) throws BaseException {
         String cctvImg = s3Service.uploadFile(multipartFile);
         PostCctvFileRes postCctvFileRes = cctvService.uploadCctvFile(cctvImg);
@@ -39,7 +39,7 @@ public class CctvController {
     @Operation(summary = "post cctv 레인 개수", description = "관리자 초기 설정 - cctv 레인 개수 전달")
     @Parameter(name = "cctvLine", description = "레인 개수", example = "4")
     @ResponseBody
-    @PostMapping("/cctv/line")
+    @PostMapping("/line")
     public BaseResponse<PostCctvLineRes> uploadLine(@RequestBody PostLineReq postLineReq) throws BaseException {
         PostCctvLineRes postCctvLineRes = cctvService.uploadLine(postLineReq.getLineNum());
         return new BaseResponse<>(postCctvLineRes);
@@ -48,7 +48,7 @@ public class CctvController {
     @Operation(summary = "post cctv 좌표", description = "관리자 초기 설정 - cctv 좌표값 전달")
     @Parameter(name = "cctvPos", description = "좌표값", example = "xPos = 21.0, yPos = 5.09")
     @ResponseBody
-    @PostMapping("/cctv/pos")
+    @PostMapping("/pos")
     public BaseResponse<PostCctvPosRes> uploadPos(@RequestBody PostCctvPosListReq postCctvPosReq) throws BaseException {
         PostCctvPosRes posList = cctvService.uploadPos(postCctvPosReq);
         return new BaseResponse<>(posList);
